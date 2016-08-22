cd27a1b0981644716de019aec87fd9c01978bd7e
Zhentao <shizhentao@gmail.com>
2016-08-22 21:59:21 +0800

    update before teaching lecture 02

--- R_demo_stat/stock.R
+++ R_demo_stat/stock.R
@@ -1,7 +1,12 @@
+# install.packages("timeDate") # if use the packages for the first time, must install it.
 library(timeDate) # load a package
 
 SS = read.csv("http://ichart.finance.yahoo.com/table.csv?s=000001.SS") 
-# download data of Shanghai stock index
+# download data of SSE composite index
+
+head(SS)
+tail(SS)
+# display the dataset
 
 sc = rev(SS$Close) # revert the time series
 
