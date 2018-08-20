142466b0333cba7a3b441aa474c8e9b91815a956
Zhentao <shizhentao@gmail.com>
2018-08-21 02:01:35 +0800

    add keras example

--- R_demo/ML_experiment_flight_delay.R
+++ R_demo/ML_experiment_flight_delay.R
@@ -5,11 +5,14 @@
 
 
 # import data
+# the original data has 2.72 million of rows.
+# The data we use is 0.0001 of the sample
+
+library(tidyr)
 d0 = read.csv("flightDelay.csv", header = TRUE)
 
 # select variable
-d0 = d0[, c("ArrDelay", "ArrDel15") ]
-
+d0 = d0[, c("ArrDelay", "CRSDepTime", "DepDelay") ] %>% drop_na()
 # data splitting
 n = nrow(d0)
 n_train = 1:round(n/2)
@@ -19,15 +22,23 @@ d_test  = d0[-n_train, ]
 
 
 # linear regression
-reg = lm( ArrDelay ~ ArrDel15, data = d_train )
+reg = lm( ArrDelay ~ ., data = d_train )
+print(reg)
 # in Azure ML studio, the split is random. That's why the results are different
 
 # prediction
-yhat = predict(reg, newdata = d_test)
+# yhat = predict(reg, newdata = d_test)
+
+# these coefficients are from the Keras (in one instance)
+
+beta_keras = as.matrix( c(-0.00252398, 0.016227794, 0.9000024 ), ncol = 1 )
+yhat = as.matrix( cbind(1, d_test[,2:3]) ) %*% beta_keras
+
 
 # evaluate the out-of-sample prediction results
-print( cbind( d_test$ArrDelay , yhat ) )
+# print( cbind( d_test$ArrDelay , yhat ) )
 
 predDiff = d_test$ArrDelay - yhat
 MSE = mean( predDiff^2, na.rm = TRUE )
 print(MSE)
+
